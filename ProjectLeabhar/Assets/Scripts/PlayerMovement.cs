b7d55eca2c0164364cde74befbc46ec7d4d112d0
Blake Calhoun <cwblakec2695@gmail.com>
2018-11-20 15:44:24 -0600

    Added basic movement and sprite
    I added a basic movement script and a basic player sprite.

---
+++ ProjectLeabhar/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody2D rb;
+    private Vector2 moveVelocity;
+
+	// Use this for initialization
+	void Start () {
+
+        rb = GetComponent<Rigidbody2D>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput.normalized * speed;//the .normalized method call allows the speed to stay the same when going diagonal
+
+	}
+
+    void FixedUpdate(){
+
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+
+    }
+
+}
