f2dc22f936af9f192d1d7b2f18696d70531076b0
Blake Calhoun <cwblakec2695@gmail.com>
2019-04-02 11:10:19 -0500

    Redid tile map
    started new tile map

--- ProjectLeabhar/Assets/Scripts/CameraFollow.cs
+++ ProjectLeabhar/Assets/Scripts/CameraFollow.cs
@@ -4,26 +4,17 @@ public class CameraFollow : MonoBehaviour
 {
 
     public Transform target;
+
     public float smoothSpeed;
-    public Vector2 maxPosition;
-    public Vector2 minPosition;
-    //public Vector3 offset;
+    public Vector3 offset;
 
     void FixedUpdate()
     {
-        if(transform.position != target.position)
-        {
-            Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed*Time.deltaTime);
+        transform.position = smoothedPosition;
 
-            targetPosition.x = Mathf.Clamp(targetPosition.x, minPosition.x, maxPosition.x);
-            targetPosition.y = Mathf.Clamp(targetPosition.y, minPosition.y, maxPosition.y);
-
-            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothSpeed);
-
-        }
-        //Vector3 desiredPosition = target.position + offset;
-        //Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed*Time.deltaTime);
-        //transform.position = smoothedPosition;
+        //transform.LookAt(target);
     }
 
 
