7b225065a8f3724788becd595f8b90db2ad87ba5
Blake Calhoun <cwblakec2695@gmail.com>
2019-04-01 21:47:51 -0500

    changed out the tileset
    New tileset with character. Also going to try and use Tiled for creating the game world.

--- ProjectLeabhar/Assets/Scripts/CameraFollow.cs
+++ ProjectLeabhar/Assets/Scripts/CameraFollow.cs
@@ -4,15 +4,26 @@ public class CameraFollow : MonoBehaviour
 {
 
     public Transform target;
-
-    public float smoothSpeed = 0.125f;
-    public Vector3 offset;
+    public float smoothSpeed;
+    public Vector2 maxPosition;
+    public Vector2 minPosition;
+    //public Vector3 offset;
 
     void FixedUpdate()
     {
-        Vector3 desiredPosition = target.position + offset;
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed*Time.deltaTime);
-        transform.position = smoothedPosition;
+        if(transform.position != target.position)
+        {
+            Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+
+            targetPosition.x = Mathf.Clamp(targetPosition.x, minPosition.x, maxPosition.x);
+            targetPosition.y = Mathf.Clamp(targetPosition.y, minPosition.y, maxPosition.y);
+
+            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothSpeed);
+
+        }
+        //Vector3 desiredPosition = target.position + offset;
+        //Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed*Time.deltaTime);
+        //transform.position = smoothedPosition;
     }
 
 
