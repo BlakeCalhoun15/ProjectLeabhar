427f7799976da52ec70a1bdf0bf0874018461f70
Blake Calhoun <cwblakec2695@gmail.com>
2019-04-20 11:49:27 -0500

    started adding a game world

--- ProjectLeabhar/ProjectLeabhar/Assets/Scripts/PlayerMovement.cs
+++ ProjectLeabhar/ProjectLeabhar/Assets/Scripts/PlayerMovement.cs
@@ -7,11 +7,12 @@ public class PlayerMovement : MonoBehaviour
     public float speed;
     private Rigidbody2D myRigidBody;
     private Vector2 change;
-
+    private Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
         myRigidBody = GetComponent<Rigidbody2D>();
     }
 
@@ -20,11 +21,22 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         change = moveInput.normalized * speed;
+        UpdateAnimationAndMove();
     }
 
-    void FixedUpdate()
+    void UpdateAnimationAndMove()
     {
-        myRigidBody.MovePosition(myRigidBody.position + change * Time.fixedDeltaTime);
+        if (change != Vector2.zero)
+        {
+            myRigidBody.MovePosition(myRigidBody.position + change * Time.fixedDeltaTime);
+            animator.SetFloat("moveX", change.x);
+            animator.SetFloat("moveY", change.y);
+            animator.SetBool("moving", true);
+        }
+        else
+        {
+            animator.SetBool("moving", false);
+        }
     }
 
 }
