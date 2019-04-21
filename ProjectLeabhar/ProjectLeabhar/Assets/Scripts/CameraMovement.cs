9d6e77fdf1258122a6dea21fdb321a03ddcb10c6
Blake Calhoun <cwblakec2695@gmail.com>
2019-04-20 23:44:38 -0500

    added new area and camera movement / room movement script

--- ProjectLeabhar/ProjectLeabhar/Assets/Scripts/CameraMovement.cs
+++ ProjectLeabhar/ProjectLeabhar/Assets/Scripts/CameraMovement.cs
@@ -4,6 +4,8 @@ public class CameraMovement : MonoBehaviour
 {
     public Transform target;
     public float smoothing;
+    public Vector2 maxPos;
+    public Vector2 minPos;
 
     // Start is called before the first frame update
     void Start()
@@ -11,7 +13,6 @@ public class CameraMovement : MonoBehaviour
         
     }
     
-    // LateUpdate always comes last
     void FixedUpdate()
     {
         if (transform.position != target.position)
@@ -21,6 +22,8 @@ public class CameraMovement : MonoBehaviour
                 target.position.y,
                 transform.position.z
                 );
+            targetPosition.x = Mathf.Clamp(targetPosition.x, minPos.x, maxPos.x);
+            targetPosition.y = Mathf.Clamp(targetPosition.y, minPos.y, maxPos.y);
             transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
         }
     }
