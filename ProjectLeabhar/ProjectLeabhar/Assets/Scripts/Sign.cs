c65113095488273eaca130657dbe2133f95d2bc7
Blake Calhoun <cwblakec2695@gmail.com>
2019-04-25 11:42:58 -0500

    added attack animations. Adding blend tree next

---
+++ ProjectLeabhar/ProjectLeabhar/Assets/Scripts/Sign.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sign : MonoBehaviour
+{
+    public GameObject dialogBox;
+    public Text dialogText;
+    public string dialog;
+    public bool playerInRange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.F) && playerInRange)
+        {
+            dialogBox.SetActive(true);
+        }
+        else
+        {
+            dialogBox.SetActive(false);
+            dialogText.text = dialog;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            playerInRange = false;
+            dialogBox.SetActive(false);
+        }
+    }
+
+}
